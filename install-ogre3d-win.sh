8f2e40e8f4dd29cad641cb0b044a0f3d65579b4e
goforbroke <go.for.broke1006@gmail.com>
2020-11-04 16:38:07 +0300

    try to run on win32 (failed)

--- install-ogre3d-win.sh
+++ install-ogre3d-win.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
 
-#rm -rf C:/ogre/
+OGRE_HOME="C:/OgreSDK"
 
-if [[ ! -d C:/ogre/ ]]; then
-  git clone --depth 1 --branch v1.12.9 https://github.com/OGRECave/ogre.git C:/ogre/
+#rm -rf ${OGRE_HOME}
+
+if [[ ! -d ${OGRE_HOME} ]]; then
+  git clone --depth 1 --branch v1.12.9 https://github.com/OGRECave/ogre.git ${OGRE_HOME}
 fi
 
-set OGRE_HOME="C:/ogre/"
-
-cd C:/ogre/
+cd ${OGRE_HOME} || exit 1
 
 (
   mkdir -p build32
-  cd build32/
+  cd build32/ || exit 1
 
   rm -rf ./*
 
-  cmake -DCMAKE_BUILD_TYPE=Release -A win32 .. -G "Visual Studio 16 2019"
+  cmake -DCMAKE_BUILD_TYPE=Release -A win32 .. #-G "Visual Studio 16 2019"
   cmake --build . --config Release
   cmake --install . --config Release
 
 )
+
+echo "Please setup env var OGRE_HOME to 'C:\OgreSDK\build32\sdk' value and reload IDE"
+echo "Please setup env var OGRE_DIR to 'C:\OgreSDK\build32\sdk\CMake' value and reload IDE"
 
 #(
 #  mkdir -p build64
